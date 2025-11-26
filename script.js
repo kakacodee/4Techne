0eb027784bf00a47b91884e9ebc2dd0471b07d4e
Thiago <thiagodgomesd892@gmail.com>
2025-11-26 08:46:12 -0300

    Adicionando a equipe

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ function mudarImagem(direction) {
     slides.style.transform = `translateX(${-Index * 100}%)`;
 }
 
+
+const membros = document.querySelector('.membros-slide');
+const membro = document.querySelectorAll('.membro');
+const totalMembros = membros.children.length;
+const membroWidth 
+
+function mudarMembro(direction) {
+    
+
+    Index += direction;
+
+    if (Index < 0) {
+        Index = totalMembros - 1;
+    } else if (Index >= totalMembros) {
+        Index = 0;
+    }
+    slides.style.transform = `translateX(${-Index * }%)`;
